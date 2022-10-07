63403d79ac8af2b076234f9d574c24c2ff0000f7
Loickp <loick.postel@enigma-school.com>
2022-10-07 10:35:26 +0100

    add api

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,28 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import { onMount } from "svelte";
+
+  let movies = [];
+
+	onMount(async () => {
+		const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=985d2210611eda826a52915c38021a1a`);
+		let movies_temp = await res.json();
+    movies = movies_temp.results
+	});
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <h1 class="my-4 text-center">The Movies</h1>
+  <div style="display: flex; flex-wrap: wrap; gap: 20px; justify-content: center; margin: 50px">
+    {#each movies as movie}
+    <div class="card" style="max-width: 300px;">
+      <img src="https://image.tmdb.org/t/p/w500{movie.poster_path}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">{movie.title}</h5>
+        <p class="card-text">{movie.overview}</p>
+        <p class="card-text">Note : <span class="fw-bold">{movie.vote_average}</span></p>
+        <a href="#" class="btn btn-primary">See more</a>
+      </div>
+    </div>
+  {/each}
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
